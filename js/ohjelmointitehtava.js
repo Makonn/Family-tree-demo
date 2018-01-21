83ee64e8d28c60aa1cebd121f6429967c98fbcdf
Petja Makkonen <petja.makkonen@hotmail.com>
2018-01-21 20:29:30 +0200

    Add files via upload

--- js/ohjelmointitehtava.js
+++ js/ohjelmointitehtava.js
@@ -15,7 +15,7 @@ $( document ).ready(function() {
   //Loads json
   loadJson();
 
-  //Search for matches
+  //Eventlistener for search
   $( "#search" ).click(function() {
     searchForMatch();
   });
@@ -29,6 +29,23 @@ $( document ).ready(function() {
 
   //Empty inputField on pageload
   $("#inputField" ).val(null);
+
+
+  //Listener for click on inputfield to show and not to show list
+  $("#inputField").click( function(event){
+        event.stopPropagation();
+        $(".proposalsUl").toggle();
+        $("#inputField" ).val(null);
+    });
+
+    $(document).click( function(){
+        $(".proposalsUl").hide();
+    });
+
+  //Listener for proposals
+  $( "#inputField" ).keyup(function() {
+    proposalFilter();
+  });
 
 });
 
@@ -44,6 +61,9 @@ function loadJson(){
 
         response = JSON.parse(this.responseText);
 
+        //Add proposal list after loading
+        addToProposals();
+
     }
 
     else {
@@ -53,13 +73,14 @@ function loadJson(){
     }
   };
 
+  //Open and send request, change proxy solution to something else
   xmlhttp.open("GET", "https://cors.io/?" + url, true);
   xmlhttp.send();
 
 }
 
 //Searches for a match
-function searchForMatch(){
+function searchForMatch(proposaInput){
 
   //Remove old information on new search
   $( ".searchResults" ).remove();
@@ -123,20 +144,20 @@ function parents(i){
       $( ".inputWrapper" ).append( "<div class=searchResults>Mother: " + mother + "</div>" );
 
     }
+
+    //Search for grandparents, father and mother index to help searching
+    grandParents(fatherIndex, motherIndex);
+    //Search for siblings, fatherindex, mother index and input array to help searching
+    siblings(fatherIndex, motherIndex, input);
+
   }
-
-  //Search for grandparents, father and mother index to help searching
-  grandParents(fatherIndex, motherIndex);
-  //Search for siblings, fatherindex, mother index and input array to help searching
-  siblings(fatherIndex, motherIndex, input);
-
 }
 
 //Search grandparents
 function grandParents(fatherIndex, motherIndex){
 
   //If has grandparents
-  if(response[fatherIndex].father != null || response[fatherIndex].mother != null || response[motherIndex].mother != null || response[fatherIndex].father != null){
+  if(response[fatherIndex - 1].father != null || response[fatherIndex - 1].mother != null || response[motherIndex - 1].mother != null || response[fatherIndex - 1].father != null){
 
     $( ".inputWrapper" ).append( resultsGrandparents );
 
@@ -144,7 +165,7 @@ function grandParents(fatherIndex, motherIndex){
     var addedInfoMother = 0;
 
     //If fathers side grandfather not null
-    if(response[fatherIndex].father != null){
+    if(response[fatherIndex - 1].father != null){
       addedInfoFather++;
       var grandfatherFatherSideIndex = response[fatherIndex - 1].father;
       var grandfatherFatherSide = response[grandfatherFatherSideIndex - 1].firstName + " " + response[grandfatherFatherSideIndex - 1].lastName;
@@ -157,7 +178,7 @@ function grandParents(fatherIndex, motherIndex){
     }
 
     //if fathers side grandmother not null
-    if(response[fatherIndex].mother != null){
+    if(response[fatherIndex - 1].mother != null){
       addedInfoFather++;
       var grandmotherFatherSideIndex = response[fatherIndex - 1].mother;
       var grandmotherFatherSide = response[grandmotherFatherSideIndex - 1].firstName + " " + response[grandmotherFatherSideIndex - 1].lastName;
@@ -170,7 +191,8 @@ function grandParents(fatherIndex, motherIndex){
     }
 
     //If mothers side mother not null
-    if(response[motherIndex].mother != null){
+    if(response[motherIndex - 1].mother != null){
+
       addedInfoMother++;
       var grandmotherMothderSideIndex = response[motherIndex - 1].mother;
       var grandmotherMotherSide = response[grandmotherMothderSideIndex - 1].firstName + " " + response[grandmotherMothderSideIndex - 1].lastName;
@@ -184,7 +206,7 @@ function grandParents(fatherIndex, motherIndex){
     }
 
     //If mothers side father not null
-    if(response[motherIndex].father != null){
+    if(response[motherIndex - 1].father != null){
       addedInfoMother++;
       var grandfatherMotherSideIndex = response[motherIndex - 1].father;
       var grandfatherMotherSide = response[grandfatherMotherSideIndex - 1].firstName + " " + response[grandfatherMotherSideIndex - 1].lastName;
@@ -201,15 +223,56 @@ function grandParents(fatherIndex, motherIndex){
 //Search for siblings
 function siblings(fatherIndex, motherIndex, input){
 
+  var added = 0;
+
   //Iterate and search for same parents
   $.each( response, function( i ) {
 
     if(response[i].father == fatherIndex && response[i].mother == motherIndex && response[i].firstName != input[0] && response[i] != input[1]){
-      $( ".inputWrapper" ).append( resultsSiblings );
-      console.log("OSUMA " + response[i].firstName + " " + response[i].lastName);
+      if(added == 0){
+        $( ".inputWrapper" ).append( resultsSiblings );
+        added++;
+      }
       $( ".inputWrapper" ).append( "<div class=searchResults>" + response[i].firstName + " " + response[i].lastName + "</div>" );
     }
 
   });
 
+}
+
+//Add proposals to list
+function addToProposals(){
+
+  $.each( response, function( i ) {
+    $(".proposalsUl").css("width", $( "#inputField" ).outerWidth());
+    $( ".proposalsUl" ).append( "<li class=proposalResults>" + response[i].firstName + " " + response[i].lastName + "</li>" );
+  });
+
+  $( ".proposalResults" ).click(function() {
+    $("#inputField" ).val($(this).text());
+    searchForMatch($(this).text());
+
+  });
+}
+
+//Filter search
+function proposalFilter() {
+
+  // Declare variables
+  var input, filter, li, i;
+
+  input = document.getElementById("inputField");
+  filter = input.value.toUpperCase();
+  li = document.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+
+    if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    }
+    else {
+      li[i].style.display = "none";
+    }
+  }
 }
